487bd62192bc71fe887e850c82be5af9528d5fc4
arunthacharuthodi <arunthacharuthodi@gmail.com>
2021-04-23 12:13:09 +0530

    payment screen cross checked

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'StorePay',
+      title: 'Store Pay',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
