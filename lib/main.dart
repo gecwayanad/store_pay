bf7a8c9c7f35dd17f851c38eeae0be4f55413578
arunthacharuthodi <arunthacharuthodi@gmail.com>
2021-04-23 12:10:35 +0530

    payment screen cross checked

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:store_pay/Screens/loginScreen.dart';
-=======
-import 'Screens/paymentScreen.dart';
->>>>>>> master
 
 void main() {
   runApp(MyApp());
@@ -13,23 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       title: 'StorePay',
-=======
-      title: 'StorePay',
->>>>>>> master
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-<<<<<<< HEAD
       home: LoginScreen(),
     );
   }
 }
 
-=======
-      home: LoginScreen(),
-    );
-  }
-}
->>>>>>> master
