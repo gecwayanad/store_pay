512896003f9ca37ee68dcc67bfaa39b45f1928e4
arunthacharuthodi <arunthacharuthodi@gmail.com>
2021-04-23 11:57:34 +0530

    login page added

--- lib/widgets/widgets.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-Widget mainAppbar(BuildContext context){
-  return AppBar(
-    title: Text("storepay"),
-
-  );
-}
