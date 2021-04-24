d68b99680bbced44beb4f3032a813d139c4bc651
amithlal200047 <amithlalmv333@gmail.com>
2021-04-24 14:58:46 +0530

    home page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Screens/paymentScreen.dart';
+import 'Screens/homeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Store Pay',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: 
-    );
+        title: 'Store Pay',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Home());
   }
 }
