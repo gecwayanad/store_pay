eaeeb0aef50851dab20bc3eae7f515f975c28a92
arunthacharuthodi <arunthacharuthodi@gmail.com>
2021-04-25 13:01:24 +0530

    payment done page added '

--- lib/Screens/paymentDone.dart
+++ lib/Screens/paymentDone.dart
@@ -1,5 +1,21 @@
 
-
+//extra added
+//------------------
+//pluggins:
+//1.googlefonts
+//2.hexcolor (to support hexa colour code)
+//
+//files:
+//null
+//
+//pending features:
+//------------------
+//1.animation for check mark
+//
+//
+////problems faced:
+//------------------
+//null
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -15,14 +31,14 @@ class _PaymentDoneState extends State<PaymentDone> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
+      body:Container( //main container
         decoration: BoxDecoration(
           color: Colors.white,
           
 
         ),
         child: Center(
-          child: Column(
+          child: Column(                                        //column inside main container
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
@@ -31,7 +47,7 @@ class _PaymentDoneState extends State<PaymentDone> {
                 decoration: BoxDecoration(
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black45,
+                      color: Colors.black45,//shadow for check button box
                       offset: const Offset(
                         0,
                         5,
@@ -46,23 +62,23 @@ class _PaymentDoneState extends State<PaymentDone> {
                     ), //BoxShadow
                   ],
                   borderRadius: BorderRadius.circular(50),
-                  color: HexColor("40BFFF")
+                  color: HexColor("40BFFF")//hex pluggin used
                   
                   
                 ),
                 child: Icon(Icons.check_rounded, size: 40,color: Colors.white,),
               ),
               SizedBox(height: 50,),
-              Text("Success", style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+              Text("Success", style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),//gooogle fonts used
                SizedBox(height: 20,),
-              Text("thank you for using Store pay",style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 12,)),),
+              Text("thank you for using Store pay",style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 12,)),),//gooogle fonts used
               SizedBox(height: 30,),
               ElevatedButton(
                 style:  ElevatedButton.styleFrom(
-                  primary: HexColor("40BFFF"),
+                  primary: HexColor("40BFFF"),        //hex pluggin used
                   padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20)
                 ),
-                onPressed: (){}, child: Text("Back To Home",style: GoogleFonts.poppins(
+                onPressed: (){}, child: Text("Back To Home",style: GoogleFonts.poppins(//gooogle fonts used
                 textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),))),
 
             ],
